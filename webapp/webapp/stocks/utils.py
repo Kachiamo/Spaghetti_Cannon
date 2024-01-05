e25ba419c3b41685a28bb8dada40d758a4213514
Devin Rosen <devinprosen@gmail.com>
2024-01-04 21:26:36 -0600

    Fix for migrating stocks

--- webapp/webapp/stocks/utils.py
+++ webapp/webapp/stocks/utils.py
@@ -2,7 +2,11 @@ from .models import Stock
 
 
 def symbol_choices():
-    values = Stock.objects.values_list('symbol', 'name')
-    # Include the symbol and name in the display
-    ret = [(item[0], f"{item[0]}: {item[1]}") for item in values]
-    return ret
+    try:
+        values = Stock.objects.values_list('symbol', 'name')
+        # Include the symbol and name in the display
+        ret = [(item[0], f"{item[0]}: {item[1]}") for item in values]
+        return ret
+    except Exception:
+        return []
+
