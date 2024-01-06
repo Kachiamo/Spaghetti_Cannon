fecfb328af2bdf4039dc522cd68db989ef17c7c1
Devin <devinprosen@gmail.com>
2024-01-05 20:40:47 -0600

    Initial pass at training logistic regression with the ':train' endpoint (#25)

---
+++ webapp/webapp/trading_models/ml_models.py
@@ -0,0 +1,15 @@
+import logging
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+from strategies.utils import STRATEGIES
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+
+
+def train_logistic_regression(X_train, y_train):
+    ml_model = LogisticRegression(solver="lbfgs", random_state=1)
+    ml_model.fit(X_train, y_train)
+    return ml_model
--- webapp/webapp/trading_models/utils.py
+++ webapp/webapp/trading_models/utils.py
@@ -1,3 +1,44 @@
+import logging
+
+from backtesting.utils import backtest
+from .ml_models import train_logistic_regression
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+
+
 ML_MODEL_CHOICES = [
-    
+    ("logistic_regression", "logistic regression")
 ]
+
+ML_MODELS = {
+    "logistic_regression": train_logistic_regression,
+}
+
+def train_trading_model(trading_model, train=0.3, period="1y"):
+    # probably not the best name, but returns a strategy with df, signals, actions, and plotting
+    strategy = backtest(trading_model.strategy, trading_model.symbol, period)
+
+    # get X_train and y_train
+    # Get the length for training data size
+    train_length = round(len(strategy.df.keys())*train)
+
+    # Clean the data for training
+    df = strategy.df.dropna()
+
+    # Get the training dataframe
+    train = df[:train_length]
+
+    # Get the name of the action column
+    y_column = f"{strategy.strategy}_Action"
+    y_train = train[y_column]
+    X_train = train.drop(columns=[y_column])
+
+    # Get the model class
+    model_class = ML_MODELS.get(trading_model.ml_model)
+
+    # Instantiate the model with the X and y train data
+    model_instance = model_class(X_train, y_train)
+
+    # Save the model and its score
+    log.critical(f"{trading_model.symbol} {trading_model.strategy} Training Data Score: {model_instance.score(X_train, y_train)}")
