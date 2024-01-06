ba0bb06e8e1ee4705020f95b3283642b39725338
Brian_Keys <b_keys06@hotmail.com>
2024-01-06 16:52:37 -0600

    adding svc

--- webapp/webapp/trading_models/ml_models.py
+++ webapp/webapp/trading_models/ml_models.py
@@ -2,7 +2,7 @@ import logging
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-
+from sklearn import svm
 from strategies.utils import STRATEGIES
 
 logging.basicConfig()
@@ -11,5 +11,11 @@ log = logging.getLogger(__name__)
 
 def train_logistic_regression(X_train, y_train):
     ml_model = LogisticRegression(solver="lbfgs", random_state=1)
+    ml_model.fit(X_train, y_train)
+    return ml_model
+
+
+def train_svc(X_train, y_train):
+    ml_model = svm.SVC()
     ml_model.fit(X_train, y_train)
     return ml_model
