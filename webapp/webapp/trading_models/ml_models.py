d4afe14a515fd23dac107d2f4254e364cac2410a
Devin Rosen <devinprosen@gmail.com>
2024-01-07 19:01:34 -0600

    Use grid search to find optimal model parameters

--- webapp/webapp/trading_models/ml_models.py
+++ webapp/webapp/trading_models/ml_models.py
@@ -1,26 +1,80 @@
 import logging
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 from sklearn import svm
-from strategies.utils import STRATEGIES
 from sklearn.ensemble import RandomForestClassifier
+from strategies.utils import STRATEGIES
 
 logging.basicConfig()
 log = logging.getLogger(__name__)
 
 
-def train_logistic_regression(X_train, y_train):
-    ml_model = LogisticRegression(solver="lbfgs", random_state=1)
-    ml_model.fit(X_train, y_train)
-    return ml_model
+def get_logistic_regression():
+    parameters = {
+        "C": [
+            1,
+            5,
+            15,
+            20,
+        ],
+        "solver": [
+            "lbfgs",
+            "liblinear",
+            "newton-cg",
+            "newton-cholesky",
+        ],
+        "max_iter": [
+            50,
+            100,
+            150,
+        ]
+    }
+    return LogisticRegression(), parameters
 
-def train_random_forest_classifier(X_train, y_train):
-    ml_model = RandomForestClassifier(n_estimators=100, random_state=1)
-    ml_model.fit(X_train, y_train)
-    return ml_model
 
-def train_svc(X_train, y_train):
-    ml_model = svm.SVC()
-    ml_model.fit(X_train, y_train)
-    return ml_model
+def get_random_forest_classifier():
+    parameters = {
+        "n_estimators": [
+            50,
+            100,
+            150,
+            200,
+        ],
+        "criterion": [
+            "gini",
+            "entropy",
+            "log_loss",
+        ],
+        "max_features": [
+            "sqrt",
+            "log2",
+        ],
+        "class_weight": [
+            "balanced",
+            "balanced_subsample",
+        ]
+    }
+    return RandomForestClassifier(), parameters
+
+
+def get_svc():
+    parameters = {
+        "kernel": [
+            "linear",
+            "rbf",
+            "sigmoid",
+        ],
+        "C": [
+            1,
+            5,
+            15,
+            20,
+        ],
+        "degree": [
+            1,
+            5,
+            10,
+            20,
+        ]
+    }
+    return svm.SVC(), parameters
