d294d0407fe7969f9b06fd94da4aca05451e6ae1
Devin <devinprosen@gmail.com>
2024-01-08 14:07:13 -0600

    Ability to order trading models (#48)

--- webapp/webapp/strategies/utils.py
+++ webapp/webapp/strategies/utils.py
@@ -11,6 +11,8 @@ from .strategies import (
 
 
 STRATEGIES = {
+    # BuyHoldTradingStrategy.id: BuyHoldTradingStrategy,
+    # DCATradingStrategy.id: DCATradingStrategy,
     SMATradingStrategy.id: SMATradingStrategy,
     RSITradingStrategy.id: RSITradingStrategy,
     ATRTradingStrategy.id: ATRTradingStrategy,
@@ -19,20 +21,10 @@ STRATEGIES = {
     BollingerBandsTradingStrategy.id: BollingerBandsTradingStrategy,
 }
 
-ALL_STRATEGIES = {
-    BuyHoldTradingStrategy.id: BuyHoldTradingStrategy,
-    DCATradingStrategy.id: DCATradingStrategy,
-    SMATradingStrategy.id: SMATradingStrategy,
-    RSITradingStrategy.id: RSITradingStrategy,
-    ATRTradingStrategy.id: ATRTradingStrategy,
-    StochasticOscillatorTradingStrategy.id: StochasticOscillatorTradingStrategy,
-    MovingAverageConvergenceDivergenceTradingStrategy. id: MovingAverageConvergenceDivergenceTradingStrategy,
-    BollingerBandsTradingStrategy.id: BollingerBandsTradingStrategy,
-}
 
 STRATEGY_CHOICES = [
-    (BuyHoldTradingStrategy.id, BuyHoldTradingStrategy.name),
-    (DCATradingStrategy.id, DCATradingStrategy.name),
+    # (BuyHoldTradingStrategy.id, BuyHoldTradingStrategy.name),
+    # (DCATradingStrategy.id, DCATradingStrategy.name),
     (SMATradingStrategy.id, SMATradingStrategy.name),
     (RSITradingStrategy.id, RSITradingStrategy.name),
     (ATRTradingStrategy.id, ATRTradingStrategy.name),
