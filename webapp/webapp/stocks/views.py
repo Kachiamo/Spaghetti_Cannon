4c394df031913a7bdbad725f849e3d391f21465a
Devin <devinprosen@gmail.com>
2024-01-08 16:07:28 -0600

    Webapp README (#49)

--- webapp/webapp/stocks/views.py
+++ webapp/webapp/stocks/views.py
@@ -1,5 +1,6 @@
 from django_filters import rest_framework as drf_filters
 from rest_framework import generics
+from rest_framework.filters import OrderingFilter
 
 from .import serializers, models, filters
 
@@ -10,4 +11,5 @@ class Stocks(generics.ListAPIView):
     serializer_class = serializers.Stock
     queryset = models.Stock.objects.all()
     filterset_class = filters.StockFilter
-    filter_backends = (drf_filters.DjangoFilterBackend,)
+    filter_backends = (drf_filters.DjangoFilterBackend, OrderingFilter, )
+    ordering_fields = ['symbol', 'name', 'country', 'ipo_year', 'sector', 'industry']
